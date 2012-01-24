8e326b0eec2905b7044d73611061d0f24241698e
Ernst von Waltzdorf <ewalzel@gmail.com>
2012-01-24 13:56:22 +0000

    Created project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LogAnalyzer::Application.config.secret_token = 'cd7477aed0a65a1935c651fa23481e361d32a7b18a4bb4a044ff9a41254d22c0a20fc26ca3742967fbee3b7c75daaf32563bcd0aed4498811c6549325230ffe6'
