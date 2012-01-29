a663c23255e3095e55a83a9a8b9caf38fd656900
Ernest Walzel <ernest.walzel@study.beds.ac.uk>
2012-01-29 21:18:51 +0000

    initial version of import_to_db working

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,16 @@
 class Log < ActiveRecord::Base
   belongs_to :investigation
   has_many :log_messages
+
+  def import_to_db
+    path='/var/tmp/access_combined.log'
+
+    file_handle = File.open(path, "r")
+    file_handle.lines.each do |line|
+      message = LogMessage.new(:log_id => id,
+                               :raw_message => line)
+      message.save
+    end
+
+  end
 end
--- app/models/log_message.rb
+++ app/models/log_message.rb
@@ -1,5 +1,6 @@
 class LogMessage < ActiveRecord::Base
   belongs_to :log
 
+  validates(:log, presence: true)
   validates_associated :log
 end
