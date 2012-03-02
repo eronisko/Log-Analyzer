94990f4e0ae43c5029235020c6d4b26c0a2e736a
Ernest Walzel <ernest.walzel@study.beds.ac.uk>
2012-03-02 16:02:59 +0000

    added comment for VALID_DATA_TYPES

--- app/models/log.rb
+++ app/models/log.rb
@@ -7,6 +7,7 @@ class Log < ActiveRecord::Base
   validates :time_bias, :numericality => true
   validates_uniqueness_of :name, :scope => :investigation_id
 
+  # The data types currently accepted by the application
   VALID_DATA_TYPES = ['plaintext']
 
   def uploaded_file=(uploaded_file)
