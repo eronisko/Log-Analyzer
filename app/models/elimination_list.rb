cce85c928ac636cf683647f4ca4003c4337fb4ad
Ernest Walzel <ernest.walzel@study.beds.ac.uk>
2012-03-04 17:49:02 +0000

    added migration file for tracking

--- app/models/elimination_list.rb
+++ app/models/elimination_list.rb
@@ -3,9 +3,8 @@ class EliminationList < ActiveRecord::Base
   validates_uniqueness_of :name
 
   def filter_log_message(log_message)
+    # TODO needs to account for \r\n as well as \n
     pattern_list.split("\n").each do |pattern|
-      logger.debug "matching against #{pattern}"
-      logger.debug pattern_list.split("\r\n").count
       if log_message.raw_message.include? pattern then
       	log_message.ignored = true
       end
