f58ff44a21a287cf51937fcbf83fc49cf2414cec
Ernest Walzel <ernest.walzel@study.beds.ac.uk>
2012-03-13 17:31:31 +0000

    Added data validation

--- app/models/message_pattern.rb
+++ app/models/message_pattern.rb
@@ -1,3 +1,6 @@
 class MessagePattern < ActiveRecord::Base
   belongs_to :source
+
+  validates :name, :pattern, presence: true
+  validates_uniqueness_of :name, scope: :source_id
 end
--- test/unit/message_pattern_test.rb
+++ test/unit/message_pattern_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class MessagePatternTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @new_pattern = MessagePattern.new(
+                        message_patterns(:client_error).attributes
+                   )
+    @new_pattern.name = "A unique pattern name"
+
+    @new_source = Source.new(sources(:apache_combined_errors).attributes)
+    @new_source.name  = "A unique source name"
+  end
+
+  test "names have to be unique within a source" do
+    @new_pattern.name = message_patterns(:client_error).name
+    assert !@new_pattern.save, 'Successfully saved a duplicate pattern'
+  end
+
+  test "names can repeat across different sources" do
+    @new_pattern.name    = message_patterns(:client_error).name
+    @new_pattern.source  = @new_source 
+
+    assert @new_pattern.save
+  end
+
+  test "has to have a name" do
+    @new_pattern.name = nil
+    assert !@new_pattern.save
+  end
+
+  test "has to have a pattern" do
+    @new_pattern.pattern = nil
+    assert !@new_pattern.save
+  end
 end
