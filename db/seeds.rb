3532c1658446dbb035077ddf3cc9162e7b778829
Ernest Walzel <ernest.walzel@study.beds.ac.uk>
2012-03-29 20:12:59 +0100

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,18 +29,50 @@ apache_2x_3x_ignore_list = IgnoreList.create(
                               description: 'Messages returning status 2xx or 3xx',
                               pattern_list: "HTTP/1.1\" 30\nHTTP/1.1\" 200")
 
+Source.delete_all
 apache_combined_source = Source.create( name: 'Apache Combined Access Error Messages',
                                         description: 'extracting error messages',
                                         timestamp_definition: '.+',
-                                        field_1_name: 'client_error',
-                                        field_1_definition: '4\w+')
+                                        field_1_name: 'ip_address',
+                                        field_1_definition: '.+',
+                                        field_2_name: 'http_method',
+                                        field_2_definition: '\w+',
+                                        field_3_name: 'category_id',
+                                        field_3_definition: '\w+',
+                                        field_4_name: 'page',
+                                        field_4_definition: '\w+'
+                                      )
+                                      
+MessagePattern.destroy_all                                      
 
-client_error_pattern = MessagePattern.create(
+client_error_pattern =  MessagePattern.create(
                           source: apache_combined_source,
                           name: 'Client error (4xx)',
-                          pattern: '.+ \[@@timestamp@@\] \".+?\" @@client_error@@',
+                          pattern: '@@ip_address@@ - - \[@@timestamp@@\]' +
+                                   ' "@@http_method@@ .+?" 4\w+ ' +
+                                   '.+_store\/@@page@@.+',
+                          category: 'Errors')
+sever_error_pattern =   MessagePattern.create(
+                          source: apache_combined_source,
+                          name: 'Server error (5xx)',
+                          pattern: '@@ip_address@@ - - \[@@timestamp@@\]' +
+                                   ' "@@http_method@@ .+?" 5\w+ ' +
+                                   '.+_store\/@@page@@.+',
                           category: 'Errors')
 
+no_error_pattern =      MessagePattern.create(
+                          source: apache_combined_source,
+                          name: 'Normal Query (2xx/3xx)',
+                          pattern: '@@ip_address@@ - - \[@@timestamp@@\]' +
+                                   ' "@@http_method@@ .+?" [23]\w+ ' +
+                                   '.+_store\/@@page@@.+',
+                          category: 'Normal')
 
-                                         
+pages_pattern =      MessagePattern.create(
+                          source: apache_combined_source,
+                          name: 'Other queries (2xx/3xx)',
+                          pattern: '@@ip_address@@ - - \[@@timestamp@@\]' +
+                                   ' "@@http_method@@ .+?" \w+ ' +
+                                   '.+_store\/@@page@@.+',
+                          category: 'Normal')
 
